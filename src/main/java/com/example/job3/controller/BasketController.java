2c2807307533e2980c9474f4b40e9f8d250f344a
syntheticdiva <alina.sheveleva2023@mail.ru>
2024-03-05 19:00:12 +0300

    feat(controller): add BasketController

---
+++ src/main/java/com/example/job3/controller/BasketController.java
@@ -0,0 +1,56 @@
+package com.example.job3.controller;
+
+import com.example.job3.dto.BasketDto;
+import com.example.job3.dto.UpdateBasketDto;
+import com.example.job3.entity.BasketEntity;
+import com.example.job3.service.BasketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/basket")
+public class BasketController {
+
+    private final BasketService basketService;
+
+    @Autowired
+    public BasketController(BasketService basketService) {
+        this.basketService = basketService;
+    }
+
+    @GetMapping("/all")
+    public List<BasketEntity> getAllBasket() {
+        return basketService.getAllBasket();
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<BasketDto> getBasketById(@PathVariable UUID uuid) {
+        BasketDto basketDto = basketService.getBasketById(uuid);
+        if (basketDto != null) {
+            return ResponseEntity.ok(basketDto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> createBasket(@RequestBody BasketDto basketDto) {
+        basketService.createBasket(basketDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{uuid}")
+    public ResponseEntity<BasketDto> updateBasket(@PathVariable UUID uuid, @RequestBody UpdateBasketDto basketDto) {
+        BasketDto updateBasket = basketService.updateBasket(basketDto);
+        if (updateBasket != null) {
+            return ResponseEntity.ok(updateBasket);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
